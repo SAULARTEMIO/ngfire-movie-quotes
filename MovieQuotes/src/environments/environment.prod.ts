b667489de041b2725fa90cbb0254d92c769655ee
SAULARTEMIO <SAULARTEMIO@GMAIL.COM>
2017-09-19 03:06:55 -0500

    SetUp Angular Fire 2

--- MovieQuotes/src/environments/environment.prod.ts
+++ MovieQuotes/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyD-MUQ67w0qrCmM3R-gQwnJtYUP1Q4uCrc",
+    authDomain: "ngf-movie-quotes-sagd.firebaseapp.com",
+    databaseURL: "https://ngf-movie-quotes-sagd.firebaseio.com",
+    projectId: "ngf-movie-quotes-sagd",
+    storageBucket: "ngf-movie-quotes-sagd.appspot.com",
+    messagingSenderId: "729906010723"
+  }
 };
--- MovieQuotes/src/environments/environment.ts
+++ MovieQuotes/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyD-MUQ67w0qrCmM3R-gQwnJtYUP1Q4uCrc",
+    authDomain: "ngf-movie-quotes-sagd.firebaseapp.com",
+    databaseURL: "https://ngf-movie-quotes-sagd.firebaseio.com",
+    projectId: "ngf-movie-quotes-sagd",
+    storageBucket: "ngf-movie-quotes-sagd.appspot.com",
+    messagingSenderId: "729906010723"
+  }
 };
