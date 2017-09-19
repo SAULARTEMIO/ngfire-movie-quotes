4bccf12c5558abd218fb7e5065d64bade21ff0c8
SAULARTEMIO <SAULARTEMIO@GMAIL.COM>
2017-09-19 02:22:06 -0500

    Improve my look

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface MovieQuote {
+  movie: string;
+  quote: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movie = 'The lego movie';
+  movieQuote: MovieQuote = {
+    'quote' : '',
+    'movie' : ''
+  }
+
+  onSubmit(): void {
+    console.log("You wish to submit", this.movieQuote);
+  }
+
+
 }
