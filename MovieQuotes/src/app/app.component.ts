727a6cdc8b5bb21e084efda0beaab2a9611f5570
SAULARTEMIO <SAULARTEMIO@GMAIL.COM>
2017-09-19 03:51:37 -0500

    Firebase read ***

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 interface MovieQuote {
   movie: string;
   quote: string;
+  $key?: string;
 }
 
 @Component({
@@ -11,27 +12,35 @@ interface MovieQuote {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  readonly quotesPath = "quotes";
+
   formMovieQuote: MovieQuote = {
     'quote' : '',
-    'movie' : ''
+    'movie' : '',
   };
 
-  movieQuotes: MovieQuote[] = [
-    {"movie": "Rocky", "quote": "Yo Adrian"},
-    {"movie": "Terminator", "quote": "I'll be back"},
-    {"movie": "Titanic", "quote": "I'm the king of the world!"},
-    {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
-  ];
+  //Local Only solution
+  // movieQuotes: MovieQuote[] = [
+  //   {"movie": "Rocky", "quote": "Yo Adrian"},
+  //   {"movie": "Terminator", "quote": "I'll be back"},
+  //   {"movie": "Titanic", "quote": "I'm the king of the world!"},
+  //   {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
+  // ];
 
-  constructor(db: AngularFireDatabase){
-
+  movieQuotesStream: FirebaseListObservable<MovieQuote[]>;
+  
+  constructor(db: AngularFireDatabase) {
+    this.movieQuotesStream = db.list(this.quotesPath);
   }
 
   onSubmit(): void {
     console.log("You wish to submit", this.formMovieQuote);
     
-    this.movieQuotes.unshift(this.formMovieQuote);
+    //Local Only solution
+    // this.movieQuotes.unshift(this.formMovieQuote);
 
     this.formMovieQuote = {
       'quote' : '',
